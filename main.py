b7f0ed9b5de38f4ba5c55e7539ae4fbf0f612974
Tomáš Turecký <t_turecky@utb.cz>
2024-12-09 11:46:28 +0100

    Snaha o získání oblasti zájmu z Trumpových tweetů.

--- main.py
+++ main.py
@@ -37,7 +37,34 @@ def main():
     dictDf = {}
     for candidate in df.candidate.unique():
         dictDf[candidate] = df.loc[df.candidate == candidate]
-    print(dictDf['Donald Trump'])
+    # print(dictDf['Donald Trump'])
+
+    trump_string = ' '.join(dictDf['Donald Trump'].text.values[1:])
+    # print(trump_string)
+    tk = nltk.TweetTokenizer()
+    trump_string_tokens = tk.tokenize(trump_string)
+    stopwords = nltk.corpus.stopwords.words('english')
+    words = [w for w in trump_string_tokens if w.isalpha() or '#' in w] #Nechává slova s hashtagy kvůli jejich důležitosti
+    words = [w for w in words if w.lower() not in stopwords]
+    words = [w for w in words if w != 'RT']
+    # text = nltk.Text(words)
+    # text.concordance("war", lines=10)
+    # finder_bi = nltk.collocations.BigramCollocationFinder.from_words(words)
+    # most_bi = finder_bi.ngram_fd.most_common(50)
+    # count = 0
+    # for bi in most_bi:
+    #     if bi[1] != count:
+    #         count = bi[1]
+    #         print(bi)
+    finder_tri = nltk.collocations.TrigramCollocationFinder.from_words(words)
+    most_tri = finder_tri.ngram_fd.most_common(300)
+    count = 0
+    for tri in most_tri:
+        if tri[1] != count:
+            count = tri[1]
+            print(tri)
+    # finder_quad = nltk.collocations.QuadgramCollocationFinder.from_words(words)
+    # print(finder_quad.ngram_fd.most_common(10))
 
 
 
